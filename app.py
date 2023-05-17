d019cdbd9b1f27a1fd29e9a1825196f3d90eed5c
Manvendra <2020pietcsmanvendra106@poornima.org>
2023-05-17 10:24:58 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask,render_template,request
+import gtts
+import os
+import playsound
+from googletrans import Translator
+
+translator = Translator()
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'POST':
+        txt = request.form.get('textt')
+        lang = request.form.get('lang')
+
+        if lang == 'English':
+           lang='en'
+        elif lang == 'Hindi':
+           lang='hi'
+        elif lang == 'German':
+           lang='de'
+        elif lang == 'Russian':
+           lang='ru'
+        elif lang == 'Spanish':
+           lang='es'
+    
+        
+        mytext=translator.translate(txt, dest=lang)
+
+        myobj = gtts.gTTS(mytext.text, lang=lang)
+
+        myobj.save("welcome.mp3")
+
+        playsound.playsound("welcome.mp3")
+
+        os.remove("welcome.mp3")
+
+    return render_template("index.html")
+
+app.run(debug=True)
